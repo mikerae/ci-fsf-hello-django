deb42d705ed51f23da46259020da824654035d8d
Mike Rae <mikerae@me.com>
2022-11-13 13:54:35 +0000

    Add Models PArt 1: add Item classd

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=, default=False)
