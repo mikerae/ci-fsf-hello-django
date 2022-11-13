085e219bc4856ada2a5afad3b3c28b08f63cd0a4
Mike Rae <mikerae@me.com>
2022-11-13 14:15:20 +0000

    Add Models part 2: create table in database, and modify model Item

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
