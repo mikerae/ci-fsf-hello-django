a1652a27a6f3bf0889680077b81fa0bbfd2626fc
Mike Rae <mikerae@me.com>
2022-11-13 13:57:06 +0000

    Add Models PArt 1: add Item class - correction

--- todo/models.py
+++ todo/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(null=False, blank=, default=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
