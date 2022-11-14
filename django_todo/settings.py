f0ffbeb6177c445699535574425e543e266fdd66
Mike Rae <mikerae@me.com>
2022-11-14 23:48:48 +0000

    Add failed push

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import env
+if os.path.exists("env.py"):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
